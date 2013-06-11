b45ee42057b5cd1cfd24b4bbcd128927941714d4
Ducv <ducv09@gmail.com>
2013-06-11 09:58:35 +0700

    update

--- smarthome/src/com/ascentcomtec/smarthouse/activities/setting/usermanager/DeleteUser.java
+++ smarthome/src/com/ascentcomtec/smarthouse/activities/setting/usermanager/DeleteUser.java
@@ -1,20 +1,19 @@
 package com.ascentcomtec.smarthouse.activities.setting.usermanager;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.GridView;
+import android.widget.EditText;
 
 import com.ascentcomtec.smarthouse.R;
 import com.ascentcomtec.smarthouse.base.BaseActivity;
-import com.ascentcomtec.smarthouse.ui.portlets.usermanage.EditUser;
 
 public class DeleteUser extends BaseActivity implements OnClickListener {
 
 	private Button confirmBt;
-	private GridView gridView;
+	private EditText pinEt;
+	private String curString;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -37,8 +36,20 @@ public class DeleteUser extends BaseActivity implements OnClickListener {
 	protected void initControl() {
 		super.initControl();
 		confirmBt = (Button) findViewById(R.id.myButtonConfirm);
-		// gridView = (GridView)findViewById(R.id.myGridViewKeypad);
-
+		pinEt = (EditText) findViewById(R.id.myEditTextAdministratorPin);
+		
+		findViewById(R.id.myButton0).setOnClickListener(this);
+		findViewById(R.id.myButton1).setOnClickListener(this);
+		findViewById(R.id.myButton2).setOnClickListener(this);
+		findViewById(R.id.myButton3).setOnClickListener(this);
+		findViewById(R.id.myButton4).setOnClickListener(this);
+		findViewById(R.id.myButton5).setOnClickListener(this);
+		findViewById(R.id.myButton6).setOnClickListener(this);
+		findViewById(R.id.myButton7).setOnClickListener(this);
+		findViewById(R.id.myButton8).setOnClickListener(this);
+		findViewById(R.id.myButton9).setOnClickListener(this);
+		findViewById(R.id.myButtonDel).setOnClickListener(this);
+		findViewById(R.id.myButtonOK).setOnClickListener(this);
 	}
 
 	@Override
@@ -58,10 +69,59 @@ public class DeleteUser extends BaseActivity implements OnClickListener {
 		case R.id.myButtonConfirm:
 			finish();
 			break;
-
+		case R.id.myButton0:
+			addWord(0);
+			break;
+		case R.id.myButton1:
+			addWord(1);
+			break;
+		case R.id.myButton2:
+			addWord(2);
+			break;
+		case R.id.myButton3:
+			addWord(3);
+			break;
+		case R.id.myButton4:
+			addWord(4);
+			break;
+		case R.id.myButton5:
+			addWord(5);
+			break;
+		case R.id.myButton6:
+			addWord(6);
+			break;
+		case R.id.myButton7:
+			addWord(7);
+			break;
+		case R.id.myButton8:
+			addWord(8);
+			break;
+		case R.id.myButton9:
+			addWord(9);
+			break;
+		case R.id.myButtonDel:
+			deleteWord();
+			break;
+		case R.id.myButtonOK:
+			finish();
+			break;
 		default:
 			break;
 		}
+	}
+
+	private void addWord(int index) {
+		curString = pinEt.getText().toString();
+		curString = curString + index;
+		pinEt.setText(curString);
+	}
+
+	private void deleteWord() {
+		curString = pinEt.getText().toString();
+		if (curString.length()>=1) {
+			curString = curString.substring(0, curString.length() - 1);
+		}
+		pinEt.setText(curString);
 	}
 
 	@Override
--- smarthome/src/com/ascentcomtec/smarthouse/activities/setting/utilitymanager/UsageAlarm.java
+++ smarthome/src/com/ascentcomtec/smarthouse/activities/setting/utilitymanager/UsageAlarm.java
@@ -3,11 +3,15 @@ package com.ascentcomtec.smarthouse.activities.setting.utilitymanager;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.EditText;
 
 import com.ascentcomtec.smarthouse.R;
 import com.ascentcomtec.smarthouse.base.BaseActivity;
 
 public class UsageAlarm extends BaseActivity implements OnClickListener {
+
+	private EditText usageAlarmEt;
+	private String curString;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +33,20 @@ public class UsageAlarm extends BaseActivity implements OnClickListener {
 	@Override
 	protected void initControl() {
 		super.initControl();
+		usageAlarmEt = (EditText) findViewById(R.id.myEditTextUsageAlarm);
+
+		findViewById(R.id.myButton0).setOnClickListener(this);
+		findViewById(R.id.myButton1).setOnClickListener(this);
+		findViewById(R.id.myButton2).setOnClickListener(this);
+		findViewById(R.id.myButton3).setOnClickListener(this);
+		findViewById(R.id.myButton4).setOnClickListener(this);
+		findViewById(R.id.myButton5).setOnClickListener(this);
+		findViewById(R.id.myButton6).setOnClickListener(this);
+		findViewById(R.id.myButton7).setOnClickListener(this);
+		findViewById(R.id.myButton8).setOnClickListener(this);
+		findViewById(R.id.myButton9).setOnClickListener(this);
+		findViewById(R.id.myButtonDel).setOnClickListener(this);
+		findViewById(R.id.myButtonOK).setOnClickListener(this);
 
 	}
 
@@ -45,9 +63,59 @@ public class UsageAlarm extends BaseActivity implements OnClickListener {
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
 		switch (v.getId()) {
+		case R.id.myButton0:
+			addWord(0);
+			break;
+		case R.id.myButton1:
+			addWord(1);
+			break;
+		case R.id.myButton2:
+			addWord(2);
+			break;
+		case R.id.myButton3:
+			addWord(3);
+			break;
+		case R.id.myButton4:
+			addWord(4);
+			break;
+		case R.id.myButton5:
+			addWord(5);
+			break;
+		case R.id.myButton6:
+			addWord(6);
+			break;
+		case R.id.myButton7:
+			addWord(7);
+			break;
+		case R.id.myButton8:
+			addWord(8);
+			break;
+		case R.id.myButton9:
+			addWord(9);
+			break;
+		case R.id.myButtonDel:
+			deleteWord();
+			break;
+		case R.id.myButtonOK:
+			finish();
+			break;
 		default:
 			break;
 		}
+	}
+
+	private void addWord(int index) {
+		curString = usageAlarmEt.getText().toString();
+		curString = curString + index;
+		usageAlarmEt.setText(curString);
+	}
+
+	private void deleteWord() {
+		curString = usageAlarmEt.getText().toString();
+		if (curString.length() >= 1) {
+			curString = curString.substring(0, curString.length() - 1);
+		}
+		usageAlarmEt.setText(curString);
 	}
 
 	@Override
